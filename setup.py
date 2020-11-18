f506a586e16c481c9dba62a24d41d7d57aaec619
Stone Zhong <shidong.zhong@oracle.com>
2020-11-18 10:58:45 -0800

    fail the job if it is canceled

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(HERE, "README.md"), "r") as f:
 # This call to setup() does all the work
 setup(
     name="spark-etl",
-    version="0.0.9",
+    version="0.0.10",
     description="Generic ETL Pipeline Framework for Apache Spark",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -25,5 +25,5 @@ setup(
     ],
     package_dir = {'': 'src'},
     packages=find_packages(where='src'),
-    install_requires=["requests", "pywebhdfs", "SQLAlchemy"],
+    install_requires=["requests", "Jinja2"],
 )
